9424d376e1cc546dd3a40306e25b06b49e09503f
Luiz Schonarth <luizcschonarth@gmail.com>
2023-12-04 16:59:07 -0300

    Added skeleton for RDT4 client and server

---
+++ RDT4_Server.py
@@ -0,0 +1,44 @@
+import argparse, RDT4
+from RDT4 import RDT4, GoBackN, SelectiveRepeat, RDT4_Protocol
+import time
+
+class RDT4_Server:
+    def __init__(self, protocol: RDT4_Protocol, port: str) -> None:
+        pass
+
+    def send_data(self, data):
+        pass
+
+    def receive_data(self) -> str:
+        pass
+
+    def recv_close_connection(self):
+        pass
+
+    def wait_close_connection(self):
+        pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='UPPER CASE server.')
+    parser.add_argument('port', help='Port.', type=int)
+    args = parser.parse_args()
+
+    timeout = 1000  # close connection if no new data within 5 seconds
+    time_of_last_data = time.time()
+    server = RDT4_Server(GoBackN, args.port)
+    try:
+        while not server.recv_close_connection():
+            msg = server.receive_data()
+            res = msg.upper()
+            server.send_data(res)
+        server.wait_close_connection()
+    except (KeyboardInterrupt, SystemExit):
+        print("Ending connection...")
+    except (BrokenPipeError, ConnectionAbortedError, ConnectionResetError):
+        print("Ending connection...")
+    finally:
+        print("Connection ended.")
+
+
+
