d6406412c53819c32eac73338a1b35808f20d6f0
Luiz Schonarth <luizcschonarth@gmail.com>
2023-12-17 22:17:19 -0300

    Implement Client and Server classes as threads

--- simulation/selective_repeat/client.py
+++ simulation/selective_repeat/client.py
@@ -9,52 +9,50 @@ path_slip = __file__.split('/')
 sys.path.append('/'.join(path_slip[0:len(path_slip)-2]))
 
 from Network import NetworkLayer
+from log_event import Logger
 from constants import *
 import utils
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Selective Repeat Client. Prints response to stdout')
-    parser.add_argument('server', help='Server.')
-    parser.add_argument('port', help='Port.', type=int)
-    parser.add_argument('file', help='File.')
-    args = parser.parse_args()
+class Client(Thread):
+    def __init__(self, server, port, file, logger: Logger=None):
+        Thread.__init__(self)
+        self.logger = logger
+        self.file = file
+        self.conn = NetworkLayer('client', server, port)
+        self.sender = Sender(self.conn, ws=WINDOW_SIZE, timeout_sec=TIMEOUT, logger=logger)
+        self.recver = Receiver(self.conn, ws=WINDOW_SIZE, logger=logger)
 
-    # Initialize state variables
-    global recv_buffer
-    recv_buffer = ''
-    conn = sender = recver = None
-    sender_t = recver_t = None
-    # Mutex to control access to recv_buffer, since the recver and main threads
-    # will both access it simutaneously
-    buffer_mutex = Lock() 
-
-    # Callback called by Receiver when data arrives
-    def recv_callback(msg: str):
+    def run(self):
+        # Initialize state variables
         global recv_buffer
-        with buffer_mutex:
-            recv_buffer += msg
+        recv_buffer = ''
 
-    try:
-        conn = NetworkLayer('client', args.server, args.port)
-        sender = Sender(conn, ws=WINDOW_SIZE, timeout_sec=TIMEOUT)
-        recver = Receiver(conn, ws=WINDOW_SIZE)
+        # Callback called by Receiver when data arrives
+        def recv_callback(msg: str):
+            global recv_buffer
+            with buffer_mutex:
+                recv_buffer += msg
+
+        # Mutex to control access to recv_buffer, since the recver and main threads
+        # will both access it simutaneously
+        buffer_mutex = Lock() 
         # Runs sender and recver threads separately
-        sender_t = Thread(target=sender.run)
-        recver_t = Thread(target=recver.run, args=[recv_callback])
+        sender_t = Thread(target=self.sender.run)
+        recver_t = Thread(target=self.recver.run, args=[recv_callback])
 
-        with open(args.file) as f:
+        with open(self.file) as f:
             buffer = f.read()
             bytes_pending = len(buffer)
 
             # Sends data in PACKET_SIZE sized chunks to server
             sender_t.start()
             for chunk in utils.getChunks(PACKET_SIZE, buffer):
-                sender.send(chunk)
+                self.sender.send(chunk)
             
-            while sender.pending_packets():
+            while self.sender.pending_packets():
                 pass
 
-            sender.stop()
+            self.sender.stop()
             sender_t.join()
             recver_t.start()
 
@@ -66,21 +64,24 @@ if __name__ == '__main__':
                 recv_buffer = ''
                 buffer_mutex.release()
 
-            recver.stop()
+            self.recver.stop()
             recver_t.join()
-            conn.disconnect()
+            self.conn.disconnect()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Selective Repeat Client. Prints response to stdout')
+    parser.add_argument('server', help='Server.')
+    parser.add_argument('port', help='Port.', type=int)
+    parser.add_argument('file', help='File.')
+    args = parser.parse_args()
+
+    try:
+        client = Client(args.server, args.port, args.file, Logger('client'))
+        client.start()
+        client.join()
+        print([e.export() for e in client.logger.events])
 
     except (Exception, KeyboardInterrupt) as err:
         print('ERROR: ' + type(err).__name__)
         print(err)
-        if conn:
-            sender.stop()
-            sender_t.join()
-            recver.stop()
-            recver_t.join()
-            conn.disconnect()
-
-    sys.stderr.write('\n')
-    sys.stderr.write(f'sender: {sender.get_stats()}\n')
-    sys.stderr.write(f'recver: {recver.get_stats()}\n')
-    sys.stderr.write(f'conn  : {conn.get_stats()}\n')
--- simulation/selective_repeat/server.py
+++ simulation/selective_repeat/server.py
@@ -9,8 +9,53 @@ path_slip = __file__.split('/')
 sys.path.append('/'.join(path_slip[0:len(path_slip)-2]))
 
 from Network import NetworkLayer
+from log_event import Logger
 from constants import *
 import utils
+
+class Server(Thread):
+    def __init__(self, server: str, port: str, logger: Logger=None):
+        Thread.__init__(self)
+        self.logger = logger
+        self.conn = NetworkLayer('server', server, port)
+        self.sender = Sender(self.conn, ws=WINDOW_SIZE, timeout_sec=TIMEOUT, logger=logger)
+        self.recver = Receiver(self.conn, ws=WINDOW_SIZE, logger=logger)
+
+    def run(self):
+        # Initialize state variables
+        global recv_buffer
+        recv_buffer = ''
+
+        # Callback called by Receiver when data arrives
+        def recv_callback(msg: str):
+            global recv_buffer
+            recv_buffer += msg
+
+        sender_t = Thread(target=self.sender.run)
+        recver_t = Thread(target=self.recver.run, args=[recv_callback])
+        recver_t.start()
+
+        while self.recver.last_recv_time == 0 or time() < self.recver.last_recv_time + TIMEOUT + 5 :
+            pass
+
+        self.recver.stop()
+        recver_t.join()
+
+        sender_t.start()
+
+        # Send captalized text as response to client
+        for chunk in utils.getChunks(PACKET_SIZE, recv_buffer):
+            self.sender.send(chunk)
+        recv_buffer = '' # Clean buffer
+
+        # Wait communication to end
+        while self.sender.pending_packets() and time() < self.sender.last_recv_time + 5:
+            pass
+
+        self.sender.stop()
+        sender_t.join()
+        self.conn.disconnect()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Selective Repeat Server.')
@@ -18,59 +63,13 @@ if __name__ == '__main__':
     parser.add_argument('port', help='Port.', type=int)
     args = parser.parse_args()
 
-    # Initialize state variables
-    global recv_buffer
-    recv_buffer = ''
-    conn = sender = recver = None
-    sender_t = recver_t = None
-
-    # Callback called by Receiver when data arrives
-    def recv_callback(msg: str):
-        global recv_buffer
-        recv_buffer += msg
-
     try:
-        conn = NetworkLayer('server', args.server, args.port)
-        sender = Sender(conn, ws=WINDOW_SIZE, timeout_sec=TIMEOUT)
-        recver = Receiver(conn, ws=WINDOW_SIZE)
+        server = Server(args.server, args.port, Logger('server'))
+        server.start()
+        server.join()
+        print([e.export() for e in server.logger.events])
 
-        # Received data from client
-        recver_t = Thread(target=recver.run, args=[recv_callback])
-        recver_t.start()
-
-        while recver.last_recv_time == 0 or time() < recver.last_recv_time + TIMEOUT + 5 :
-            pass
-
-        recver.stop()
-        recver_t.join()
-
-        sender_t = Thread(target=sender.run)
-        sender_t.start()
-
-        # Send captalized text as response to client
-        for chunk in utils.getChunks(PACKET_SIZE, recv_buffer):
-            sender.send(chunk)
-        recv_buffer = '' # Clean buffer
-
-        # Wait communication to end
-        while sender.pending_packets() and time() < sender.last_recv_time + 5:
-            pass
-
-        sender.stop()
-        sender_t.join()
-        conn.disconnect()
             
     except (Exception, KeyboardInterrupt) as err:
         print('ERROR: ' + type(err).__name__)
         print(err)
-        if conn:
-            sender.stop()
-            sender_t.join()
-            recver.stop()
-            recver_t.join()
-            conn.disconnect()
-
-    sys.stderr.write('\n')
-    sys.stderr.write(f'sender: {sender.get_stats()}\n')
-    sys.stderr.write(f'recver: {recver.get_stats()}\n')
-    sys.stderr.write(f'conn  : {conn.get_stats()}\n')
